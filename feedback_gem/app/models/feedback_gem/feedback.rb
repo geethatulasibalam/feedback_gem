657393c06250aeaff1527d4ba163e39cdedee408
geethatulasi-nyros <geethatulasi.nyros@gmail.com>
2019-10-30 09:01:33 +0530

    completed with modals and search

--- feedback_gem/app/models/feedback_gem/feedback.rb
+++ feedback_gem/app/models/feedback_gem/feedback.rb
@@ -2,7 +2,7 @@ module FeedbackGem
   class Feedback < ApplicationRecord
   	validates :name,:email,:subject,:description,presence: true
   	def self.search(searchname)
-			where('name LIKE ?',searchname)
+			where('name LIKE ?',"%#{searchname}%")
 		end
   end
 end
--- feedback_gem/lib/feedback_gem/version.rb
+++ feedback_gem/lib/feedback_gem/version.rb
@@ -1,3 +1,3 @@
 module FeedbackGem
-  VERSION = '0.2.8'
+  VERSION = '0.2.9'
 end
