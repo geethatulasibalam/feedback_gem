ff82f7529eee7efae8f6086a2bbf4d15d2e6c04d
geethatulasi-nyros <geethatulasi.nyros@gmail.com>
2019-10-31 19:08:41 +0530

    after mentioned changes

--- app/assets/javascripts/feedback_gem/feedbacks.js
+++ app/assets/javascripts/feedback_gem/feedbacks.js
@@ -5,50 +5,78 @@
 $(document).ready(function () {
 	$("#new_feedback").validate({
 		rules: {
-		"feedback[name]": {required: true},
-		"feedback[email]": {required: true, email: true},
-		"feedback[subject]": {required: true},
-		"feedback[description]": {required: true}
+		"feedback[name]": {required: true,maxlength:30},
+		"feedback[email]": {required: true, email: true,maxlenght:25},
+		"feedback[subject]": {required: true,maxlenth:30},
+		"feedback[description]": {required: true,maxlength:200}
 		},
 		messages:{
 			"feedback[name]":{
-				required:"Please enter name"
+				required:"Please enter name",
+				maxlength: "The name should not exceed than 30 characters"
 			},
 			"feedback[email]":{
-				required:"Please enter email"
+				required:"Please enter email",
+				maxlength: "The email should not exceed than 25 characters"
 			},
 			"feedback[subject]":{
-				required:"Please enter subject"
+				required:"Please enter subject",
+				maxlength: "The subject should not exceed than 30 characters"
 			},
 			"feedback[description]":{
-				required:"Please enter description"
+				required:"Please enter description",
+				maxlength: "The description should not exceed than 200 characters"
 			},
 		}
 	});
 	$(".edit_feedback").validate({
 		rules: {
-		"feedback[name]": {required: true},
-		"feedback[email]": {required: true, email: true},
-		"feedback[subject]": {required: true},
-		"feedback[description]": {required: true}
+		"feedback[name]": {required: true,maxlength:30},
+		"feedback[email]": {required: true, email: true,maxlenght:25},
+		"feedback[subject]": {required: true,maxlength:30},
+		"feedback[description]": {required: true,maxlength:200}
 		},
 		messages:{
 			"feedback[name]":{
-				required:"Please enter name"
+				required:"Please enter name",
+				maxlength: "The name should not exceed than 30 characters"
 			},
 			"feedback[email]":{
-				required:"Please enter email"
+				required:"Please enter email",
+				maxlength: "The email should not exceed than 25 characters"
 			},
 			"feedback[subject]":{
-				required:"Please enter subject"
+				required:"Please enter subject",
+				maxlength: "The subject should not exceed than 30 characters"
 			},
 			"feedback[description]":{
-				required:"Please enter description"
+				required:"Please enter description",
+				maxlength: "The description should not exceed than 200 characters"
 			},
 		}
 	});
 	$('#feedbacks_table').DataTable({
 		"order": [[ 0, "desc" ]]
+	});
+	$('#new_feedback_cancel').click(function(){
+		$('#new_feedback')[0].reset();
+		var validator = $("#new_feedback").validate();
+		validator.resetForm();
+	});
+	$('#edit_feedback_cancel').click(function(){
+		$('.edit_feedback')[0].reset();
+		var validator = $(".edit_feedback").validate();
+		validator.resetForm();
+	});
+	$('#new').on('hidden.bs.modal', function (e) {
+    $('#new_feedback')[0].reset();
+		var validator = $("#new_feedback").validate();
+		validator.resetForm();
+	});
+	$('.edit_feedback_form').on('hidden.bs.modal', function (e) {
+   $('.edit_feedback')[0].reset();
+		var validator = $(".edit_feedback").validate();
+		validator.resetForm();
 	});
 });
  
--- lib/feedback_gem/version.rb
+++ lib/feedback_gem/version.rb
@@ -1,3 +1,3 @@
 module FeedbackGem
-  VERSION = '0.3.4'
+  VERSION = '0.3.6'
 end
